2bddd0661c9a2216b493eb071db37750065f4d5d
Tom Leitch <tom@leitchy.com>
2011-10-01 14:50:57 +0100

    henriks merge

--- app.js
+++ app.js
@@ -133,7 +133,8 @@ app.post('/talk', function(req, res){
 });
 
 
-app.listen(8154);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 
