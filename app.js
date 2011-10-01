887866d18c74f9d306eaaa172c58a1d75c84aa12
Tom Leitch <tom@leitchy.com>
2011-10-01 14:46:18 +0100

    fixed package

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ app.post('/talk', function(req, res){
 });
 
 
-app.listen(8080);
+app.listen(8154);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 
